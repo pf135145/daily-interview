18fc60540b371b653f855a5414ba2e9d42f52037
pf135145 <962914907@qq.com>
2022-04-12 00:20:49 +0800

    feat: 04.11-home

--- js-code/Scheduler.js
+++ js-code/Scheduler.js
@@ -1,32 +1,31 @@
+// JS 实现一个带并发限制的异步调度器 Scheduler，保证同时运行的任务最多有两个
+// 完善下面的代码中的 Scheduler 类，使得一下程序能正确输出（题目见每日一题）
+
+// 思路：在 promiseCreator 外面包一层，用来计算当前正在执行的个数，
+// 每次添加的函数的时候判断正在执行的数量处理下一个函数是执行还是推到栈里，函数执行后顺序执行下一个即可
+
 class Scheduler {
   constructor() {
-    this.schedulers = []
-    this.onDoing = 0
-    this.raf = null
+    this.queue = []
+    this.doing = 0
   }
-
   add(promiseCreator) {
-    this.schedulers.push(promiseCreator)
-    this.startLoop()
-  }
-
-  startLoop() {
-    if (this.onDoing < 2 && this.schedulers.length > 0) {
-      let fn = this.schedulers.shift()
-      fn()
-      this.onDoing ++
+    const tempFunc = () => {
+      this.doing++
+      promiseCreator()
+        .then(() => {
+          this.doing--
+          if (this.queue.length) {
+            let fn = this.queue.shift()
+            fn()
+          }
+      })
     }
-    if (this.schedulers.length) {
-      this.raf = requestAnimationFrame(this.startLoop);
+    if (this.doing < 2) {
+      tempFunc()
     } else {
-      this.endLoop()
+      this.queue.push(tempFunc)
     }
-    
-  }
-
-  endLoop() {
-    this.raf && cancelAnimationFrame(this.raf);
-    this.raf = null;
   }
 }
 
@@ -46,3 +45,4 @@ addTask(1000, '1')
 addTask(500, '2')
 addTask(300, '3')
 addTask(400, '4')
+addTask(600, '5')
--- js-code/koa-next.js
+++ js-code/koa-next.js
@@ -1,24 +1,4 @@
-// 实现koa洋葱模型
-
-// koa洋葱模型
-// const app = require('./');
-// app.use((next) => {
-//   console.log(1);
-//   next();
-//   console.log(2);
-// });
-// app.use((next) => {
-//   console.log(3);
-//   next();
-//   console.log(4);
-// });
-// app.use((next) => {
-//   console.log(5);
-//   next();
-//   console.log(6);
-// });
-// app.compose();
-// 输出结果  1 3 5 6 4 2
+// 实现koa洋葱模型，题干见每日一题
 
 // 提示：先实现以下效果
 const arr = [];
